810aa9e9b9d143879c5e84249160657d9baedbc4
v36373bb <ben.barrow@student.manchester.ac.uk>
2021-11-24 02:35:07 +0000

    Login Screen Functionalised

--- loginScreen.py
+++ loginScreen.py
@@ -1,6 +1,10 @@
+# Title screen background from https://pixabay.com/, title text generated with https://cooltext.com/, torches and tombstone taken from https://pngimg.com/search_image
+
+# Resolution: 1280x720 
+
 import tkinter as tk
-import os
-  
+import os 
+
 def data(line):
 	data = ""
 	trigger = 0
@@ -13,89 +17,111 @@ def data(line):
 	return(data)
 
 def login():
-    usernameFinal= username.get()
-    passwordFinal= password.get()
-    errorOutput.config(text="")
+	global user
+	user = usernameInput.get()
+	password = passwordInput.get()
 
-    if usernameFinal == "" or usernameFinal.isspace():
-    	errorOutput.config(text="Username cannot be empty")
-    elif passwordFinal == "" or passwordFinal.isspace():
-    	errorOutput.config(text="Password cannot be empty")
-    elif os.path.exists(os.path.join(os.getcwd(), "accounts", usernameFinal + ".txt")):
-    	newFile = open(os.path.join(os.getcwd(), "accounts", usernameFinal + ".txt"), "r")
-    	for i, line in enumerate(newFile):
-    		if i == 0:
-    			usernameFile = data(line)
-    		elif i == 1:
-    			passwordFile = data(line)
-    		elif i > 1:
-    			break
+	errorOutput.config(text="")
 
-    	newFile.close()
+	if user == "" or user.isspace():
+		errorOutput.config(text="Username cannot be empty")
+	elif password == "" or password.isspace():
+		errorOutput.config(text="Password cannot be empty")
+	elif os.path.exists(os.path.join(os.getcwd(), "accounts", user + ".txt")):
+		newFile = open(os.path.join(os.getcwd(), "accounts", user + ".txt"), "r")
+		for i, line in enumerate(newFile):
+			if i == 0:
+				usernameFile = data(line)
+			elif i == 1:
+				passwordFile = data(line)
+			elif i > 1:
+				break
 
-    	if usernameFile != usernameFinal + "\n":
-    		errorOutput.config(text="Username is incorrect")
-    	elif passwordFile != passwordFinal + "\n":
-    		errorOutput.config(text="Password is incorrect")
-    	else:
-    		errorOutput.config(text="Welcome Adventurer!")
+		newFile.close()
 
+		if usernameFile != user + "\n":
+			errorOutput.config(text="Username is incorrect")
+		elif passwordFile != password + "\n":
+			errorOutput.config(text="Password is incorrect")
+		else:
+			errorOutput.config(text="Welcome Adventurer!")
+			loginWindow.destroy()
+	else:
+		errorOutput.config(text="Username does not exist")
 
-
-    else:
-    	errorOutput.config(text="Username does not exist")
-    	
 def create():
-	usernameFinal= username.get()
-	passwordFinal= password.get()
+	global user
+
+	user = usernameInput.get()
+	password = passwordInput.get()
 	errorOutput.config(text="")
 
 	if os.path.isdir("accounts") == False:
 		os.mkdir("accounts")
 	
-	if usernameFinal == "" or usernameFinal.isspace():
+	if user == "" or user.isspace():
 		errorOutput.config(text="Username cannot be empty")
-	elif passwordFinal == "" or passwordFinal.isspace():
+	elif password == "" or password.isspace():
 		errorOutput.config(text="Password cannot be empty")
-	elif os.path.exists(os.path.join(os.getcwd(), "accounts", usernameFinal + ".txt")):
+	elif os.path.exists(os.path.join(os.getcwd(), "accounts", user + ".txt")):
 		errorOutput.config(text="Account Already Exists")
 	else:
-		newFile = open(os.path.join(os.getcwd(), "accounts", usernameFinal + ".txt"), "w")
-		newFile.write("Username:"+ usernameFinal + "\n")
-		newFile.write("Password:"+ passwordFinal + "\n")
+		newFile = open(os.path.join(os.getcwd(), "accounts", user + ".txt"), "w")
+		newFile.write("Username:"+ user + "\n")
+		newFile.write("Password:"+ password + "\n")
 		newFile.close()
 		errorOutput.config(text="Account Successfully Created!")
-		
-loginWindow= tk.Tk()
-loginWindow.geometry("600x600")
-loginWindow.title("RunEscape Login")
-loginWindow.resizable(0, 0)
-  
-username= tk.StringVar()
-password= tk.StringVar()
 
-nameLabel= tk.Label(loginWindow, text = "Username:")
-nameFetch= tk.Entry(loginWindow,textvariable = username)
+def loginScreen():
+	global loginWindow		
+	loginWindow= tk.Tk()
+	loginWindow.geometry("1280x720")
+	loginWindow.title("Run Escape Login")
+	loginWindow.resizable(0, 0)
 
-passwordLabel= tk.Label(loginWindow, text = "Password:")
-passwordFetch= tk.Entry(loginWindow, textvariable = password, show = '*')
- 
-loginButton= tk.Button(loginWindow,text = "Submit", command = login)
-accountButton= tk.Button(loginWindow,text = "Create Account", command = create)
+	bg_login = tk.PhotoImage(file = "background_login.png")
+	bg = tk.Label(loginWindow, image=bg_login)
+	bg.place(x=0, y=0)
 
-errorOutput = tk.Label(loginWindow, text = "")
-  
-nameLabel.grid(row=1,column=1)
-nameFetch.grid(row=1,column=2)
-passwordLabel.grid(row=2,column=1)
-passwordFetch.grid(row=2,column=2)
-loginButton.grid(row=4,column=2)
-accountButton.grid(row=5,column=2)
-errorOutput.grid(row=6,column=2)
+	global usernameInput
+	global passwordInput
+	  
+	usernameInput= tk.StringVar()
+	passwordInput= tk.StringVar()
 
-loginWindow.grid_rowconfigure(0, weight=1)
-loginWindow.grid_rowconfigure(7, weight=1)
-loginWindow.grid_columnconfigure(0, weight=1)
-loginWindow.grid_columnconfigure(3, weight=1)
-  
-loginWindow.mainloop()
+	nameLabel= tk.Label(loginWindow, text = "Username:")
+	nameFetch= tk.Entry(loginWindow,textvariable = usernameInput)
+
+	passwordLabel= tk.Label(loginWindow, text = "Password:")
+	passwordFetch= tk.Entry(loginWindow, textvariable = passwordInput, show = '*')
+	 
+	loginButton= tk.Button(loginWindow,text = "Submit", command = login)
+	loginWindow.bind("<Return>", login)
+	accountButton= tk.Button(loginWindow,text = "Create Account", command = create)
+
+	global errorOutput
+
+	errorOutput = tk.Label(loginWindow, text = "")
+	  
+	nameLabel.grid(row=1,column=1)
+	nameFetch.grid(row=1,column=2)
+	passwordLabel.grid(row=2,column=1)
+	passwordFetch.grid(row=2,column=2)
+	loginButton.grid(row=4,column=2)
+	accountButton.grid(row=5,column=2)
+	errorOutput.grid(row=6,column=2)
+
+	loginWindow.grid_rowconfigure(0, weight=1)
+	loginWindow.grid_rowconfigure(7, weight=1)
+	loginWindow.grid_columnconfigure(0, weight=1)
+	loginWindow.grid_columnconfigure(3, weight=1)
+	  
+	loginWindow.mainloop()
+	
+	return  
+
+def mainScreen(user):
+	print(user)
+
+loginScreen()
+mainScreen(user)
